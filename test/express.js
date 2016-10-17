b74496e66432c18161c44325d10c0c66cf450b67
Tim Creasy <timcreasy@me.com>
2016-10-17 12:07:20 -0500

    Added post and delete test

--- test/express.js
+++ test/express.js
@@ -7,8 +7,22 @@ describe('express', () => {
   it('should respond with Hello World', (done) => {
     request(app)
       .get('/')
-      .expect(200)
-      .expect('Hello World')
+      .expect('Content-Type', /json/)
+      .end(done);
+  });
+  it('should respond with an id', (done) => {
+    request(app)
+      .post('/addresses')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        _id: 123,
+      })
+      .end(done);
+  });
+  it('should respond with 204 on delete', (done) => {
+    request(app)
+      .delete('/addresses/123')
+      .expect(204)
       .end(done);
   });
 });
